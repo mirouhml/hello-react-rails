1d820f3d8b36fae111d2d27407710561cd67ef77
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-07-04 19:58:00 +0100

    Change the link.

--- app/javascript/services/fetchGreeting.js
+++ app/javascript/services/fetchGreeting.js
@@ -1,5 +1,5 @@
 const getGreeting = async () => {
-  const response = await fetch('http://127.0.0.1:3000/greeting');
+  const response = await fetch('/greeting');
   const data = await response.json();
   return data.message;
 };
