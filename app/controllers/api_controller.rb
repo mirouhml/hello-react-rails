52453d140b62b271593fdf23e213fa50d04a0447
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-07-04 20:37:41 +0100

    Change the json returned by the API to return only the message.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,6 +3,6 @@ class ApiController < ApplicationController
 
   def random_greeting
     greeting = Greeting.find(Greeting.pluck(:id).sample)
-    json_response(greeting)
+    json_response({message: greeting.message})
   end
 end
--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,8 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-const environment = require('./environment')
+const environment = require('./environment');
 
-module.exports = environment.toWebpackConfig()
+const SpeedMeasurePlugin = require('speed-measure-webpack-plugin');
+const smp = new SpeedMeasurePlugin();
+
+module.exports = smp.wrap(environment.toWebpackConfig());
