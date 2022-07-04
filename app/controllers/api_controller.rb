8c1fc0c8a26780e78629bd20aaf74f97a3e7d507
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-07-04 14:57:24 +0100

    Setup react with rails, and setup greetings api.

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,9 @@
+class ApiController < ApplicationController
+  
+  include Response
+
+  def random_greeting
+    greeting = Greeting.find(Greeting.pluck(:id).sample)
+    json_response(greeting)
+  end
+end
